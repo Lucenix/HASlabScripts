8d54377e45fe8e3cc6fa4b9486dc3aabd1e23e85
Lucenix <andrelucenaferreira@gmail.com>
2024-10-04 11:27:32 +0100

    Fix setup_dataset

--- pytorch/setup_dataset.sh
+++ pytorch/setup_dataset.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 SCRATCH="/home/gsd/andrelucena/imagenet"
+SCRIPT_DIR="$(dirname -- "$0")"
+echo "Script found in $SCRIPT_DIR"
 
 cd $SCRATCH
 unzip imagenet-object-localization-challenge.zip
 mv ILSVRC/Data/CLS-LOC/* ./
 cd val
-wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh > run.sh
-sh run.sh
-rm run.sh
+sh $SCRIPT/structure_val_folder.sh
 rm LOC*
 rm -rf ILSVRC
