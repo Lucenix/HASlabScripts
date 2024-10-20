11eba04de549b331bbc55a44022adb048af75237
Lucenix <andrelucenaferreira@gmail.com>
2024-10-20 15:21:39 +0100

    Aurora Scripts for Control testing

--- pytorch/models/GpuTestModelScreen.sh
+++ pytorch/models/GpuTestModelScreen.sh
@@ -2,14 +2,20 @@
 
 SCRATCH="/home/gsd/andrelucena"
 MODEL_DIR="$SCRATCH/DL_pytorch_models"
-DATA_DIR="$SCRATCH/imagenet"
+DATA_DIR="/home/gsd/goncalo/imagenet_subset"
 VENV_DIR="$SCRATCH/pytorch_venv"
-STAT_DIR="$SCRATCH/statistics/control/"
-MODEL="resnet152"
-N_EPOCHS=3
-BATCH_SIZE=32
+STAT_DIR="$SCRATCH/statistics/control_subset"
+MODEL="alexnet"
+N_EPOCHS=2
+BATCH_SIZE=1024
 
-#spawn dstat process
+# deactivate grafana agents
+sudo systemctl stop pmcd
+
+# create statistics directory
+mkdir $STAT_DIR
+
+#spawn process
 # --$1: process identifier
 # --$2: path to the output file
 function spawn-dstat-process {
@@ -17,22 +23,30 @@ function spawn-dstat-process {
         screen -S $1 -d -m $MODEL_DIR/dstat.py -tcdrnmg --noheaders --output $2
 }
 
-# Join dstat process
+function spawn-nvidia-process {
+        echo "utils::spawn-nvidia-smi-process"
+        screen -S $1 -d -m nvidia-smi --query-gpu=timestamp,name,pci.bus_id,temperature.gpu,utilization.gpu,utilization.memory --format=csv -f $2 -l 1
+}
+
+# Join process
 # --$1: process identifier
-function join-dstat-process {
-        echo "utils::join-dstat-process"
+function join-process {
+        echo "utils::join-process"
         screen -X -S $1 stuff "^C"
 }
 
+# activate venv
 source "${VENV_DIR}/bin/activate"
-echo "PYTHONPATH is ${PYTHONPATH}"
 
 # spawn dstat
-spawn-dstat-process run $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.csv
+spawn-dstat-process dstat $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.csv
+# spawn nvidia
+spawn-nvidia-process nvidia $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE\_gpu.csv
 
-time python3 $MODEL_DIR/main.py -a $MODEL --epochs $N_EPOCHS --batch-size $BATCH_SIZE $DATA_DIR > $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.out
+{ time python3 $MODEL_DIR/main.py -a $MODEL --epochs $N_EPOCHS --batch-size $BATCH_SIZE $DATA_DIR > $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.out ; } 2>> $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.out ;
 
-# join dstat
-join-dstat-process run
+# join processes
+join-process dstat
+join-process nvidia
 
 #python3 ../../dstat.py -cdnm --output ./dstat_arm_output
