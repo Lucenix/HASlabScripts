c01e077a4e10f9c56d6b726130cb1d23a8b025c3
Andre Ferreira <lucenix@slurm.cluster.lsd.di.uminho.pt>
2024-10-23 17:55:44 +0100

    Collect with full nvidia command

--- pytorch/run_models/slurm/SlurmControlModelRun.sh
+++ pytorch/run_models/slurm/SlurmControlModelRun.sh
@@ -4,6 +4,7 @@
 #SBATCH --ntasks-per-node=1         # number of MPI task per node
 #SBATCH --account=haslab
 #SBATCH --time=48:00:00 
+#SBATCH --partition=rtx4060
 
 export SCRIPT_DIR="$(dirname -- "$0")"
 SCRATCH="/home/lucenix"
@@ -12,6 +13,7 @@ DSTAT_PATH="$SCRATCH/HASlabScripts/pytorch/python/dstat.py"
 DATA_DIR="/projects/a97485/imagenet_subset"
 VENV_DIR="$SCRATCH/pytorch_venv"
 STAT_DIR="$SCRATCH/statistics/control_subset"
+SCREEN_PATH="$SCRATCH/bin/screen"
 # model is defined in main
 MODEL=resnet50
 N_EPOCHS=2
@@ -19,6 +21,8 @@ BATCH_SIZE=64
 
 # deactivate grafana agents
 sudo systemctl stop pmcd
+sudo systemctl stop pmlogger
+sudo systemctl stop pmproxy
 
 module load Python/3.11.2-GCCcore-12.2.0-bare CUDA/11.7.0
 
@@ -28,34 +32,37 @@ mkdir $STAT_DIR
 #spawn process
 # --$1: process identifier
 # --$2: path to the output file
-spawn-dstat-process() {
-        echo "utils::spawn-dstat-process"
-        screen -S $1 -d -m $DSTAT_PATH -tcdrnmg --noheaders --output $2
+spawn_dstat_process () 
+{
+        echo "utils::spawn_dstat_process"
+        $SCREEN_PATH -S $1 -d -m $DSTAT_PATH -tcdrnmg --noheaders --output $2
 }
 
-spawn-nvidia-process() {
-        echo "utils::spawn-nvidia-smi-process"
-        screen -S $1 -d -m nvidia-smi --query-gpu=timestamp,name,pci.bus_id,temperature.gpu,utilization.gpu,utilization.memory --format=csv -f $2 -l 1
+spawn_nvidia_process () 
+{
+        echo "utils::spawn-nvidia_smi_process"
+        $SCREEN_PATH -S $1 -d -m nvidia-smi --query-gpu=timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv,nounits -f $2 -l 1
 }
 
 # Join process
 # --$1: process identifier
-join-process() {
-        echo "utils::join-process"
-        screen -X -S $1 stuff "^C"
+join_process() 
+{
+        echo "utils::join_process"
+        $SCREEN_PATH -X -S $1 stuff "^C"
 }
 
 # activate venv
 source "${VENV_DIR}/bin/activate"
 # spawn dstat
-#spawn-dstat-process dstat $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.csv
+spawn_dstat_process dstat $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.csv
 # spawn nvidia
-#spawn-nvidia-process nvidia $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE\_gpu.csv
+spawn_nvidia_process nvidia $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE\_gpu.csv
 
 { time python3 $MAIN_PATH --epochs $N_EPOCHS --batch_size $BATCH_SIZE $DATA_DIR > $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.out ; } 2>> $STAT_DIR/$MODEL\_$N_EPOCHS\_$BATCH_SIZE.out ;
 
 # join processes
-#join-process dstat
-#join-process nvidia
+join_process dstat
+join_process nvidia
 
 #python3 ../../dstat.py -cdnm --output ./dstat_arm_output
