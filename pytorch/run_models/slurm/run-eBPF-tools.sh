839e9e4f27b5fb79585549580a0758ec3da3a669
Lucenix <andrelucenaferreira@gmail.com>
2024-10-29 23:05:35 +0000

    'A SLURM Love Story'

---
+++ pytorch/run_models/slurm/run-eBPF-tools.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+tools=()
+libbpf_tools=()
+
+for file in bpftrace-tools/*; do 
+    if [ -f "$file" ]; then 
+        tools+=( "$(basename "$file" .bt)" )
+    fi 
+done
+
+for file in libbpf-tools/*; do 
+    if [ -f "$file" ]; then 
+        libbpf_tools+=( "$(basename "$file")" )
+    fi 
+done
+
+
+start_tools() {
+	OUTPUT=/projects/a97485/statistics/eBPFs_subset/eBPF_output_$1
+	mkdir -p "$OUTPUT"
+    mkdir -p pids
+	echo $(pwd)
+    for tool in "${tools[@]}"
+    do
+        tool_executable="bpftrace-tools/$tool.bt"
+        sudo bpftrace $tool_executable > "$OUTPUT/$tool" 2>&1 &
+        echo $! > pids/$tool.pid
+        echo "Started $tool (pid: $!)"
+    done
+
+	for tool in "${libbpf_tools[@]}"
+	do
+        tool_executable="libbpf-tools/$tool.bt"
+        sudo $tool_executable > "$OUTPUT/$tool" 2>&1 &
+        echo $! > pids/$tool.pid
+        echo "Started $tool (pid: $!)"
+    done
+}
+
+stop_tools() {
+  for tool in "${tools[@]}"
+  do
+    pid=$(cat pids/$tool.pid)
+    echo "Stopping $tool (pid: $pid)"
+    sudo kill -SIGTERM $pid
+  done
+
+  echo "Waiting for tools to stop..."
+  for tool in "${tools[@]}"
+  do
+    pid=$(cat pids/$tool.pid)
+    tail --pid=$pid -f /dev/null
+    echo "Stopped $tool"
+  done
+	
+  echo ${libbpf_tools[@]}
+  for tool in "${libbpf_tools}"
+  do
+	pid=$(cat pids/$tool.pid)
+	tail --pid=$pid -f /dev/null
+	echo "Stopped $tool (libbpf)"		
+  done
+
+  rm -r pids/
+}
+
+if [ "$1" == "start" ]; then
+	if [ -z "$2" ]
+		then 
+			start_tools default
+		else
+			start_tools $2
+	fi
+elif [ "$1" == "stop" ]; then
+    stop_tools
+else
+    echo "Usage: $0 start|stop"
+fi
---
+++ pytorch/run_models/slurm/run_single_node_eBPFs.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+HOSTNAME=$(hostname | cut -d '.' -f 1)
+echo "I am $HOSTNAME!"
+
+# deactivate grafana agents
+sudo systemctl stop pmcd
+sudo systemctl stop pmlogger
+sudo systemctl stop pmproxy
+
+module load Python/3.11.2-GCCcore-12.2.0-bare CUDA/11.7.0 ncurses
+
+# create statistics directory
+export STAT_DIR="/projects/a97485/statistics/eBPFs_subset"
+mkdir $STAT_DIR
+
+#spawn process
+# --$1: process identifier
+# --$2: path to the output file
+spawn_dstat_process () 
+{
+        echo "utils::spawn_dstat_process"
+        echo $2
+        $SCREEN_PATH -S $1 -d -m $DSTAT_PATH -tcdrnmg --noheaders --output $2
+}
+
+spawn_nvidia_process () 
+{
+        echo "utils::spawn-nvidia_smi_process"
+        echo $2
+        $SCREEN_PATH -S $1 -d -m nvidia-smi --query-gpu=timestamp,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv,nounits -f $2 -l 1
+}
+
+# activate venv
+source "${VENV_DIR}/bin/activate"
+# mkdir
+mkdir $STAT_DIR/$MODEL\_$N_NODES\_$N_EPOCHS\_$BATCH_SIZE\_$SAVE_EVERY
+# spawn dstat
+spawn_dstat_process dstat $STAT_DIR/$MODEL\_$N_NODES\_$N_EPOCHS\_$BATCH_SIZE\_$SAVE_EVERY/$HOSTNAME.csv
+# spawn nvidia
+spawn_nvidia_process nvidia $STAT_DIR/$MODEL\_$N_NODES\_$N_EPOCHS\_$BATCH_SIZE\_$SAVE_EVERY/$HOSTNAME\_gpu.csv
+# start eBPFs
+./run-eBPF-tools.sh start $MODEL\_$N_EPOCHS\_$BATCH_SIZE\_$SAVE_EVERY
+
+{ time torchrun \
+--nnodes $N_NODES \
+--nproc_per_node 1 \
+--rdzv_id $2 \
+--rdzv_backend c10d \
+--rdzv_endpoint $1:29500 \
+$MAIN_PATH --epochs $N_EPOCHS --save_every 1 $DATA_DIR > $STAT_DIR/$MODEL\_$N_NODES\_$N_EPOCHS\_$BATCH_SIZE\_$SAVE_EVERY/$HOSTNAME.out ; } 2>> $STAT_DIR/$MODEL\_$N_NODES\_$N_EPOCHS\_$BATCH_SIZE\_$SAVE_EVERY/$HOSTNAME.out ;
+
+join_process() 
+{
+        echo "utils::join_process"
+        $SCREEN_PATH -X -S $1 stuff "^C"
+}
+
+join_process dstat
+join_process nvidia
+./run-tools.sh stop
