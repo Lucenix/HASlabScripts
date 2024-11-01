38111cfd32c41a4d08691ccd8fe6335588a60d1f
hiddenduck <cmachado0032@gmail.com>
2024-11-01 12:14:47 +0000

    minor changes to run eBPF script

--- pytorch/run_models/slurm/run-eBPF-tools.sh
+++ pytorch/run_models/slurm/run-eBPF-tools.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 tools=()
-libbpf_tools=()
-
-for file in bpftrace-tools/*; do 
+for file in tools/*; do 
     if [ -f "$file" ]; then 
-        tools+=( "$(basename "$file" .bt)" )
-    fi 
-done
-
-for file in libbpf-tools/*; do 
-    if [ -f "$file" ]; then 
-        libbpf_tools+=( "$(basename "$file")" )
+        tools+=( "$(basename "$file")" )
     fi 
 done
 
@@ -21,23 +13,12 @@ start_tools() {
 	echo $(pwd)
     for tool in "${tools[@]}"
     do
-        tool_executable="bpftrace-tools/$tool.bt"
-        $SCREEN_PATH -S $tool -d -m bash -c "sudo bpftrace $tool_executable > $OUTPUT/$tool 2>&1"
-        pid=$($SCREEN_PATH -ls | awk "/\.$tool\t/ {print strtonum(\$1)}")
+        tool_executable="tools/$tool"
+        screen -S $tool -d -m -L -Logfile $OUTPUT/$tool bash -c "sudo $tool_executable"
+        pid=$(screen -ls | awk "/\.$tool\t/ {print strtonum(\$1)}")
         echo $pid > $OUTPUT/pids/$tool.pid
         echo "Started $tool (pid: $pid)"
     done
-
-	for tool in "${libbpf_tools[@]}"
-	do
-#-L -Logfile $OUTPUT/$tool 
-        tool_executable="libbpf-tools/$tool"
-		$SCREEN_PATH -S $tool -d -m bash -c "sudo $tool_executable"
-		pid=$($SCREEN_PATH -ls | awk "/\.$tool\t/ {print strtonum(\$1)}")
-        echo $pid > $OUTPUT/pids/$tool.pid
-        echo "Started $tool (pid: $pid)"
-
-	done
 }
 
 stop_tools() {
@@ -47,37 +28,18 @@ stop_tools() {
   do
     pid=$(cat $OUTPUT/pids/$tool.pid)
     echo "Stopping $tool (pid: $pid)"
-    $SCREEN_PATH -X -S $tool stuff "^C"
+    screen -X -S $tool stuff "^C"
   done
-  
-  for tool in "${libbpf_tools[@]}"
-   do
-	pid=$(pgrep -u root $tool)
-    echo "Stopping $tool (pid: $pid)"
-    $SCREEN_PATH -X -S $tool stuff '^C'
-	#pid=$($SCREEN_PATH -ls | awk "/\.$tool\t/ {print strtonum(\$1)}")
-	#sudo kill -INT -$pid
-   done
 
   echo "Waiting for tools to stop..."
   for tool in "${tools[@]}"
   do
-   pid=$(cat $OUTPUT/pids/$tool.pid)
-	
+	pid=$(cat $OUTPUT/pids/$tool.pid)
     tail --pid=$pid -f /dev/null
     echo "Stopped $tool"
   done
 
-  for tool in "${libbpf_tools[@]}"
-  do
-   pid=$(cat $OUTPUT/pids/$tool.pid)
-	
-    tail --pid=$pid -f /dev/null
-    echo "Stopped $tool"
-  done
-   
   rm -r $OUTPUT/pids/
-
 }
 
 if [ "$1" == "start" ]; then
@@ -95,5 +57,5 @@ elif [ "$1" == "stop" ]; then
             stop_tools $2
     fi
 else
-    echo "Usage: $0 start|stop"
+    echo "Usage: $0 start|stop [OUTPUT_PATH]"
 fi
