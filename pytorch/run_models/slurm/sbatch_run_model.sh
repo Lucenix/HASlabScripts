590fa466b042b8ee38c273c976703b938c3094d9
Andre Ferreira <lucenix@slurm.cluster.lsd.di.uminho.pt>
2024-11-18 16:08:17 +0000

    Scripts fixed in slurm

--- pytorch/run_models/slurm/sbatch_run_model.sh
+++ pytorch/run_models/slurm/sbatch_run_model.sh
@@ -8,43 +8,49 @@ export SCREEN_PATH="$SCRATCH/bin/screen"
 export DATA_DIR="$SCRATCH/imagenet_subset"
 export VENV_DIR="$SCRATCH/pytorch_venv"
 export PLOT_DIR="$SCRATCH/HASLabScripts/eBPFs-tools/parser"
-export STAT_DIR="/projects/a97485/statistics/test/resnet50_2_2_64_1_false/aurora04%"
 
 if [ -z $1 ] ; then
-        export MODEL="resnet50"
-else
-        export MODEL=$1
-fi
-if [ -z $2 ] ; then
-        export N_NODES=2
-else
-        export N_NODES=$2
-fi
-if [ -z $3 ] ; then
-        export N_EPOCHS=2
-else
-        export N_EPOCHS=$3
-fi
-if [ -z $4 ] ; then
-        export BATCH_SIZE=64
-else
-        export BATCH_SIZE=$4
-fi
-if [ -z $5 ] ; then
-        export SAVE_EVERY=1
-else
-        export SAVE_EVERY=$5
-fi
-if [ -z $6 ] ; then
-        export LOG="false"
-else
-        export LOG=$6
-fi
-if [ -z $7 ] ; then
         export SINGLE_NODE_SCRIPT="$SCRATCH/HASLabScripts/pytorch/run_models/slurm/run_single_node_screen.sh"
 else
-        export SINGLE_NODE_SCRIPT=$7
+        export SINGLE_NODE_SCRIPT=$1
 fi
+if [ -z $2 ] ; then
+        export STAT_DIR="$SCRATCH/statistics/test"
+else
+        export STAT_DIR="$SCRATCH/statistics/$2"
+fi
+if [ -z $3 ] ; then
+        export MODEL="resnet50"
+else
+        export MODEL=$3
+fi
+if [ -z $4 ] ; then
+        export N_NODES=2
+else
+        export N_NODES=$4
+fi
+if [ -z $5 ] ; then
+        export N_EPOCHS=2
+else
+        export N_EPOCHS=$5
+fi
+if [ -z $6 ] ; then
+        export BATCH_SIZE=64
+else
+        export BATCH_SIZE=$6
+fi
+if [ -z $7 ] ; then
+        export SAVE_EVERY=1
+else
+        export SAVE_EVERY=$7
+fi
+if [ -z $8 ] ; then
+        export LOG="false"
+else
+        export LOG=$8
+fi
+
+export TEST_TITLE=$MODEL\_$N_NODES\_$N_EPOCHS\_$BATCH_SIZE\_$SAVE_EVERY\_$LOG
 
 SLURM_NUMBER="$(sbatch -n $N_NODES -N $N_NODES Run_Model_Slurm.sh | awk '{print $4}')"
 
