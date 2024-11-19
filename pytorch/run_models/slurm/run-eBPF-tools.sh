99aef7e99035c132b56f0ab6d565d779388bcb45
Carlos Machado <74628557+hiddenduck@users.noreply.github.com>
2024-11-19 23:25:40 +0000

    changing some eBPF files

--- pytorch/run_models/slurm/run-eBPF-tools.sh
+++ pytorch/run_models/slurm/run-eBPF-tools.sh
@@ -13,14 +13,7 @@ start_tools() {
 	echo $(pwd)
     for tool in "${tools[@]}"
     do
-
-        if [ "$tool" == "cachestat" ]; then
-            tool_executable="tools/$tool -T"
-        else
-            tool_executable="tools/$tool"
-        fi
-
-		$SCREEN_PATH -S $tool -d -m -L -Logfile $OUTPUT/$tool bash -c "sudo $tool_executable"
+		$SCREEN_PATH -S $tool -d -m -L -Logfile $OUTPUT/$tool bash -c "sudo bpftrace $tool_executable"
         pid=$($SCREEN_PATH -ls | awk "/\.$tool\t/ {print strtonum(\$1)}")
         echo $pid > $OUTPUT/pids/$tool.pid
         echo "Started $tool (pid: $pid)"
